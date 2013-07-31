5a8d06850d7a9e4589a981f8849537dcd20a6796
Darren Kopp <darrenkopp@gmail.com>
2013-07-31 00:12:21 -0600

    2013 support?
    this versioning scheme sucks

--- SassyStudio.2012/SassyStudioPackage.cs
+++ SassyStudio.2012/SassyStudioPackage.cs
@@ -9,7 +9,7 @@ using SassyStudio.Options;
 namespace SassyStudio
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "0.4", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", "0.5", IconResourceID = 400)]
     [GuidAttribute(Guids.guidSassyStudioPkgString)]
     [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
     [ProvideOptionPage(typeof(ScssOptions), "Sassy Studio", "SCSS", 101, 102, true, new string[] { "CSS", "SCSS" })]
